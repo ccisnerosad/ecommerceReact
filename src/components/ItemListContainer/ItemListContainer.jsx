eb874c2b14569922f06b3b5c5cd2e8ea8bf697da
Carlos Cisneros <54935043+ccisnerosad@users.noreply.github.com>
2023-07-08 23:37:30 -0600

    Contenedor de items

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,8 @@
+function ItemListContainer({ greeting }) {
+  return (
+    <div className="ItemListContainer text-center mt-3">
+      <h1>{greeting}</h1>
+    </div>
+  );
+}
+export default ItemListContainer;
