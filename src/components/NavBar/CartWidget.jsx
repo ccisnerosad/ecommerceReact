5b9185bec27096f79dafa8e3ffcd039cd9647ce4
Carlos Cisneros <54935043+ccisnerosad@users.noreply.github.com>
2023-07-08 23:37:19 -0600

    Carrito

---
+++ src/components/NavBar/CartWidget.jsx
@@ -0,0 +1,15 @@
+function CartWidget(){
+        return(
+                <>
+                <div className="d-flex">
+                        <div className="p-2 bd-highlight">
+                                <a href="#" className="bi bi-cart4"><span class="badge text-bg-secondary">4</span></a>
+                        </div>
+                </div>
+                </>
+
+        )
+}
+
+
+export default CartWidget;
